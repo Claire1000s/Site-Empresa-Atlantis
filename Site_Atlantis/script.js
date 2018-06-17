1deba354eb82bf7b7a06038f157647ceb50f7473
Leticia Ferreira <claire_thousands@hotmail.com>
2018-06-17 20:58:18 -0300

    Add files via upload

---
+++ Site_Atlantis/script.js
@@ -0,0 +1,7 @@
+function abrirServicos()
+{
+	var janela = document.createElement("div");
+	janela.setAttribute("class", "servicos_lista");
+	janela.setAttribute("style", "display: block;");
+	document.body.appendChild(janela);
+}
